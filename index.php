8901df4a179b1507409880c6fbf396dfc51b4491
NICOLAS-1011 <nicolasandresespitiavillamil@gmail.com>
2022-09-29 18:39:01 -0500

    Front-2

--- index.php
+++ index.php
@@ -20,9 +20,9 @@
             <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
         </a>
         <ul id="cont" class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a id="a" href="/index.html" class="nav-link px-2 link-secondary"><strong>INICIO</strong></a></li>
-            <li><a id="a" href="/productos.html" class="nav-link px-2 link-dark"><strong>PRODUCTOS</strong></a></li>
-            <li><a id="a" href="/categorias.html" class="nav-link px-2 link-dark"><strong>CATEGORIAS</strong></a></li>
+            <li><a id="a" href="./index.php" class="nav-link px-2 link-secondary"><strong>INICIO</strong></a></li>
+            <li><a id="a" href="./productos.php" class="nav-link px-2 link-dark"><strong>PRODUCTOS</strong></a></li>
+            <li><a id="a" href="./categorias.php" class="nav-link px-2 link-dark"><strong>CATEGORIAS</strong></a></li>
         </ul>
     </header>
 
